5dc4ec736ce6efdc3f4e91afa8f864b82ecb9fb0
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-07-15 19:50:35 +0900

    define account table

--- ent/schema/account.go
+++ ent/schema/account.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Account holds the schema definition for the Account entity.
 type Account struct {
@@ -9,7 +14,25 @@ type Account struct {
 
 // Fields of the Account.
 func (Account) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("id").
+			Comment("ID").
+			Immutable().
+			NotEmpty().
+			Unique(),
+		field.Time("created_at").
+			Comment("Created at").
+			Immutable().
+			Default(time.Now),
+		field.Time("updated_at").
+			Comment("Updated at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+		field.String("email").
+			Comment("Email").
+			NotEmpty().
+			Unique(),
+	}
 }
 
 // Edges of the Account.
