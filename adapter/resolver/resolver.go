1e99d16e29b8cc7592d8386a459d5717adabd9ad
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-07-20 00:19:11 +0900

    setup ent client to resolver

--- adapter/resolver/resolver.go
+++ adapter/resolver/resolver.go
@@ -1,7 +1,23 @@
 package resolver
 
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/Pranc1ngPegasus/ent-gqlgen-practice/ent"
+	"github.com/Pranc1ngPegasus/ent-gqlgen-practice/graph/generated"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	client *ent.Client
+}
+
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return generated.NewExecutableSchema(
+		generated.Config{
+			Resolvers: &Resolver{client},
+		},
+	)
+}
